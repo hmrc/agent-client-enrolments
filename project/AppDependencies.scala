950fd3499a37cc4273542dabb68a0ca5996bea6b
Inbar Vernia <104011282+inbar-vernia@users.noreply.github.com>
2022-04-29 14:09:55 +0100

    GG-6200 : Updated library dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,18 +4,18 @@ import sbt._
 object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "5.20.0"
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "5.24.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %%  "bootstrap-test-play-28"     % "5.20.0"              % "test, it",
+    "uk.gov.hmrc"             %%  "bootstrap-test-play-28"     % "5.24.0"              % "test, it",
     "com.typesafe.play"       %%  "play-test"                  % current               % "test, it",
     "org.mockito"             %% "mockito-scala-scalatest"     % "1.17.0"              % "test, it",
     "org.scalatestplus.play"  %%  "scalatestplus-play"         % "5.1.0"               % "test, it",
     "org.pegdown"             %   "pegdown"                    % "1.6.0"               % "test, it",
     "com.vladsch.flexmark"    % "flexmark-all"                 % "0.36.8"              % "test, it",
     "com.github.tomakehurst"  %   "wiremock-jre8"              % "2.32.0"              % "test, it",
-    "uk.gov.hmrc"             %%  "service-integration-test"   % "1.2.0-play-28"       % "test, it",
+    "uk.gov.hmrc"             %%  "service-integration-test"   % "1.3.0-play-28"       % "test, it",
     "com.fasterxml.jackson.module" %% "jackson-module-scala"   % "2.13.1"              % "test, it"
   )
 
