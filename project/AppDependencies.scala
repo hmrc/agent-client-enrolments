ffc16920e3c3639c246955312d7dd6526c7dfd52
Ming Sun <ming.sun@digital.hmrc.gov.uk>
2022-08-18 14:54:53 +0100

    GG-6418 : tidy up actions from internal PRA checks

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,13 +4,13 @@ import sbt._
 object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "5.24.0"
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "7.0.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %%  "bootstrap-test-play-28"     % "5.24.0"              % "test, it",
+    "uk.gov.hmrc"             %%  "bootstrap-test-play-28"     % "7.0.0"              % "test, it",
     "com.typesafe.play"       %%  "play-test"                  % current               % "test, it",
-    "org.mockito"             %% "mockito-scala-scalatest"     % "1.17.0"              % "test, it",
+    "org.mockito"             %% "mockito-scala-scalatest"     % "1.17.12"              % "test, it",
     "org.scalatestplus.play"  %%  "scalatestplus-play"         % "5.1.0"               % "test, it",
     "org.pegdown"             %   "pegdown"                    % "1.6.0"               % "test, it",
     "com.vladsch.flexmark"    % "flexmark-all"                 % "0.36.8"              % "test, it",
