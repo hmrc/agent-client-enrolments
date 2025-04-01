652726a5507e61a314aa26b50381a45d47564f10
Ian Walton <51008396+ian-1@users.noreply.github.com>
2025-04-01 14:19:48 +0100

    APB-9326 - update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,13 +3,15 @@ import sbt._
 
 object AppDependencies {
 
+  private val bootstrapVersion = "8.6.0"
+
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % "8.3.0"
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"                     %%  "bootstrap-test-play-30"     % "8.3.0"              % Test,
-    "org.mockito"                     %%  "mockito-scala-scalatest"    % "1.17.30"            % Test
+    "uk.gov.hmrc"                     %%  "bootstrap-test-play-30"     % bootstrapVersion     % Test,
+    "org.mockito"                     %%  "mockito-scala-scalatest"    % "1.17.37"            % Test
   )
 
 }
